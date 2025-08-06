4d2911384aef5a8a4da9d754b247f1ffad03aef6
Cesar Luis Rodriguez Solera <devcesarco@outlook.es>
2025-08-06 16:30:52 -0500

    feat: implement FileSystemDataSource for logging functionality

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,33 +1,36 @@
-interface CheckServiceUseCase{
-    execute( url: string ):Promise<boolean>;
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repositoty";
+
+interface CheckServiceUseCase {
+  execute(url: string): Promise<boolean>;
 }
 
 type successCallback = () => void;
-type ErrorCallback = (error:string) => void;
+type ErrorCallback = (error: string) => void;
 
 export class CheckService implements CheckServiceUseCase {
+  constructor(
+    private readonly logRepository: LogRepository,
+    private readonly successCallback: successCallback,
+    private readonly errorCallback: ErrorCallback
+  ) {}
 
-    constructor(
-        private readonly successCallback: successCallback,
-        private readonly errorCallback: ErrorCallback
-    ) {
-
+  public async execute(url: string): Promise<boolean> {
+    try {
+      const req = await fetch(url);
+      if (!req.ok) {
+        throw new Error(`Failed to fetch ${url}: ${req.statusText}`);
+      }
+      const log = new LogEntity(`service ${url} working`, LogSeverityLevel.low);
+      this.logRepository.SaveLog(log);
+      this.successCallback();
+      return true;
+    } catch (error) {
+      const errorMessage = `${error}`;
+      const log = new LogEntity(errorMessage, LogSeverityLevel.low);
+      this.logRepository.SaveLog(log);
+      this.errorCallback(errorMessage);
+      return false;
     }
-
-    public async execute( url:string ): Promise<boolean> {
-
-        try {
-            const req = await fetch( url );
-            if ( !req.ok ) {
-                throw new Error(`Failed to fetch ${url}: ${req.statusText}`);
-            }
-            this.successCallback();
-            return true;
-        } catch (error) {
-            console.error("Error checking URL:", error);
-            this.errorCallback(error instanceof Error ? error.message : "Unknown error");
-            return false;
-            
-        }
-    }
+  }
 }
