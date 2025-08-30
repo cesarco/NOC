c22292fafe07ab13fb27f9f3fe4b382ba3c313d0
Cesar Luis Rodriguez Solera <devcesarco@outlook.es>
2025-08-29 19:54:39 -0500

    feat: agregar conexión a MongoDB y refactorizar estructura de archivos

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import {envs} from "./config/plugins/envs.plugin";
+import { MongoDatabase } from "./data/mongo";
 import { ServerApp } from "./presentation/server";
 
 (async () => {
   main();
 })();
 
-function main() {
-  ServerApp.start();
+async function main() {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME
+  });
+  //ServerApp.start();
 }
