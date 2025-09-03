3edd0250a67dcd1b91e096ac2674d8b990fe9d18
Cesar Luis Rodriguez Solera <devcesarco@outlook.es>
2025-09-02 21:06:12 -0500

    feat: agregar creación y registro de logs en la aplicación

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import {envs} from "./config/plugins/envs.plugin";
 import { MongoDatabase } from "./data/mongo";
+import LogModel from "./data/mongo/models/log.model";
 import { ServerApp } from "./presentation/server";
 
 (async () => {
@@ -11,5 +12,16 @@ async function main() {
     mongoUrl: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME
   });
+
+  // crear un  coleccion = tables, documento = registro
+  const newLog = await LogModel.create({
+    message: "Test message desde Mongo",
+    origin: "App.ts",
+    level: "low"
+  })
+
+  await newLog.save();
+  console.log(newLog);
+  
   //ServerApp.start();
 }
