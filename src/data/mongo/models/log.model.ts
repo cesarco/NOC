dd02a052337e072f386f54550881dbafdae0af7c
Cesar Luis Rodriguez Solera <devcesarco@outlook.es>
2025-09-02 20:59:41 -0500

    feat: agregar modelo de log con esquema de mongoose

---
+++ src/data/mongo/models/log.model.ts
@@ -0,0 +1,20 @@
+/**
+ * Log model
+ * level: string;
+ * message: string;
+ * origin: string;
+ * createdAt: Date;
+ */
+
+import mongoose from "mongoose";
+
+const logSchema = new mongoose.Schema({
+  message: { type: String, required: true },
+  origin: { type: String, },
+  level: { type: String, enum: ['low', 'medium', 'high'], required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const LogModel = mongoose.model("Log", logSchema);
+
+export default LogModel;
